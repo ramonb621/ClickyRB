eeed594070b999d353a02bce13958954db529d2c
Ramon Barajas <ramonb621@yahoo.com>
2019-10-23 20:48:29 -0700

    got images from giphy displaying with ul list for top of page

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Game from './components/Game';
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+// import './App.css';
+import pictures from "./pictures.json";
+
+class App extends Component {
+  state = {
+    pictures,
+    score: 0,
+    tscore: 0
+  };
+  selectPic = id => {
+    const pictures = this.state.pictures.filter(picture => picture.id === id);
+
+    this.setState({ pictures });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>
+          <ul>
+            <li className="game-name">Click Game</li>
+            <li>Click an image to start game!</li>
+            <li className="score">Score: {this.state.score} <span className="topscore">| Top Score: {this.state.tscore}</span></li>
+          </ul>
+          </Title>
+          {this.state.pictures.map(picture => (
+            <Game
+            id={picture.id}
+            image={picture.image}
+            />
+          ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
